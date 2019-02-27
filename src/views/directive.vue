84f411cb217d8513a0167bb439f17adf7b2ab790
zhangbx <13128814051@163.com>
2019-02-27 19:16:07 +0800

    IMP: 代码编辑器

--- src/views/directive.vue
+++ src/views/directive.vue
@@ -4,18 +4,34 @@
       <input type="text" v-focus style="margin-right: 20px;">
       <el-button @click="test='hello world'" v-auth>click</el-button>
     </el-card>
-
+    <editor v-model="content" @init="editorInit" lang="html" theme="chrome" width="500" height="500"></editor>
   </div>
 </template>
 
 <script>
+import editor from 'vue2-ace-editor'
 export default {
+  components: { editor },
   name: 'directive',
   data () {
     return {
-      test: 'just a test'
+      test: 'just a test',
+      content: ''
     }
-  }
+  },
+  mounted () {
+    console.log('editor', editor)
+  },
+  methods: {
+      editorInit: function () {
+          require('brace/ext/language_tools') //language extension prerequsite...
+          require('brace/mode/html')
+          require('brace/mode/javascript')    //language
+          require('brace/mode/less')
+          require('brace/theme/chrome')
+          require('brace/snippets/javascript') //snippet
+      }
+  },
 }
 </script>
 
